ea92c3991de94b14587949a7908030a1e7debfb0
HJiang <33575549+hjiangcpp@users.noreply.github.com>
2025-04-21 19:54:28 +0800

    Use event.key.keysym.sym for keycode in keyboard event switch
    * Use event.key.keysym.sym for keycode in keyboard event switch
    
    * Use event.key.keysym.sym for keycode in keyboard event switch

--- gamesrc/gamedisplay.cpp
+++ gamesrc/gamedisplay.cpp
@@ -63,17 +63,17 @@ void Game::handleEvents() {
         is_running_ = false;
         break;
       case SDL_EVENT_KEY_DOWN:
-        switch ((event.key.scancode)) {
-          case SDL_SCANCODE_UP:
+        switch ((event.key.keysym.sym)) {
+          case SDLK_UP:
             snake_.newDirection(gamestatus::Direction::UP);
             break;
-          case SDL_SCANCODE_DOWN:
+          case SDLK_DOWN:
             snake_.newDirection(gamestatus::Direction::DOWN);
             break;
-          case SDL_SCANCODE_LEFT:
+          case SDLK_LEFT:
             snake_.newDirection(gamestatus::Direction::LEFT);
             break;
-          case SDL_SCANCODE_RIGHT:
+          case SDLK_RIGHT:
             snake_.newDirection(gamestatus::Direction::RIGHT);
             break;
           default:
